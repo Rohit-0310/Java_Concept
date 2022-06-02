18c1cb40400c4f0c9c846a5997c34e9e649b60a4
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-02 17:17:01 +0530

    If Statement and Loops

---
+++ PrimitiveDataType/src/com/premitivedatatyp/java/IfStatementLoops.java
@@ -0,0 +1,19 @@
+package com.premitivedatatyp.java;
+
+public class IfStatementLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int k = 20;
+		
+		if(k > 20) {
+			System.out.println(1);
+		}
+		else if(k > 10) {
+			System.out.println(2);
+		}
+
+	}
+
+}
