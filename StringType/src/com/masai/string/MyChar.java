cdfaa95f70ef78b16c1e4c0aa86bd9f0ae8e9f0b
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-03 14:50:04 +0530

    Vowel or not

--- StringType/src/com/masai/string/MyChar.java
+++ StringType/src/com/masai/string/MyChar.java
@@ -1,5 +1,34 @@
 package com.masai.string;
 
 public class MyChar {
+	private char ch;
+	
 
+	public MyChar(char ch) {
+		// TODO Auto-generated constructor stub
+		this.ch = ch;
+	}
+
+
+	public boolean isVowel() {
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+				ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+			return true;
+		}
+		{
+			return false;
+		}
+		
+	}
+
+
+	public boolean isAlphabet() {
+		if((ch >='a' && ch<='z') || (ch >='A' && ch<='Z')) {
+			return true;
+		}
+		{
+			return false;
+		}
+	}
+	
 }
--- StringType/src/com/masai/string/MyCharRunner.java
+++ StringType/src/com/masai/string/MyCharRunner.java
@@ -4,7 +4,10 @@ public class MyCharRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MyChar myChar = new MyChar('1');
+		System.out.println(myChar.isVowel());
+		System.out.println(myChar.isAlphabet());
 	}
 
 }
