d49bc6a713c077dca47e029ba581794db16a8cfa
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-05 10:21:35 +0530

    StudentRunner

--- Arrays_Session/src/com/masai/arrays/StudentRunner.java
+++ Arrays_Session/src/com/masai/arrays/StudentRunner.java
@@ -1,9 +1,28 @@
 package com.masai.arrays;
 
+import java.math.BigDecimal;
+
 public class StudentRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] marks = {97,99,100};
+		
+		Student student = new Student("Mohan", marks);
+		
+		int number = student.getNumberOfMarks();
+		System.out.println("Number Of Marks"+ number);
+		
+		int maxMarks = student.getMaximumMarks();
+		System.out.println("Sum of Marks"+ maxMarks);
+		
+		int mimMarks = student.getMinimumMarks();
+		System.out.println("Sum of Marks"+ minMarks);
+		
+		
+		BigDecimal average = student.getAverageMarks();
+		
+		
 
 	}
 
