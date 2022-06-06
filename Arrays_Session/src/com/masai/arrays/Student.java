b3d1fa5f528eaccd5020e04fe865d44b6c242a65
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-06 14:23:05 +0530

    student marks operation

--- Arrays_Session/src/com/masai/arrays/Student.java
+++ Arrays_Session/src/com/masai/arrays/Student.java
@@ -1,5 +1,53 @@
 package com.masai.arrays;
 
 public class Student {
+	
+	private String name;
+	private int[] marks;
+	
+	
+	public Student(String name, int[] marks) {
+		this.name = name;
+		this.marks = marks;
+	}
 
+
+	public int getNumberOfMarks() {
+		return marks.length;
+	}
+
+
+	public int getTotalSumOfMarks() {
+		int sum = 0;
+//		for(int i = 0; i < marks.length; i++) {
+//			sum = sum + marks[i]
+//		}
+		
+		for(int x : marks) {
+			sum = sum + x;
+		}
+		return sum;
+	}
+
+
+	public int getMaximumMarks() {
+		int maximum = Integer.MIN_VALUE;
+		for(int x : marks) {
+			if(x > maximum) {
+				maximum = x;
+			}
+		}
+		return maximum;
+	}
+
+
+	public int getMinimumMarks() {
+		int minimum = Integer.MAX_VALUE;
+		for(int x : marks) {
+			if(x < minimum) {
+				minimum = x;
+			}
+		}
+		return minimum;
+	}
 }
--- Arrays_Session/src/com/masai/arrays/StudentRunner.java
+++ Arrays_Session/src/com/masai/arrays/StudentRunner.java
@@ -11,19 +11,19 @@ public class StudentRunner {
 		Student student = new Student("Mohan", marks);
 		
 		int number = student.getNumberOfMarks();
-		System.out.println("Number Of Marks"+ number);
+		System.out.println("Number Of Marks "+ number);
 		
 		int sum = student.getTotalSumOfMarks();
-		System.out.println("Sum of Marks"+ sum);
+		System.out.println("Sum of Marks "+ sum);
 		
 		int maxMark = student.getMaximumMarks();
-		System.out.println("Max Marks"+ maxMarks);
+		System.out.println("Max Marks "+ maxMark);
 		
-		int mimMark = student.getMinimumMarks();
-		System.out.println("Minimum Marks"+ minMarks);
+		int minMark = student.getMinimumMarks();
+		System.out.println("Minimum Marks "+ minMark);
 		
 		
-		BigDecimal average = student.getAverageMarks();
+//		BigDecimal average = student.getAverageMarks();
 		
 		
 
