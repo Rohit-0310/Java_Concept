8271e9ab2e13d4883a1c4022c581fdbabd09c1a3
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-07 15:03:26 +0530

    area and perimeter of rectangle

---
+++ OOPS_Concepts/src/com/masai/oop/RectangleRunner.java
@@ -0,0 +1,19 @@
+package com.masai.oop;
+
+public class RectangleRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle rectangle = new Rectangle(13,32);
+		System.out.println(rectangle);
+		rectangle.setWidth(25);
+		System.out.println(rectangle.area());
+		System.out.println(rectangle.perimeter());
+		
+		System.out.println(rectangle);
+		
+
+	}
+
+}
