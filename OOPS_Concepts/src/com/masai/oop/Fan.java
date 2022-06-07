62cad69e361f4bbdc7ccfdec3f41ed98e40f028e
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-07 14:39:22 +0530

    Oops concept

---
+++ OOPS_Concepts/src/com/masai/oop/Fan.java
@@ -0,0 +1,23 @@
+package com.masai.oop;
+
+public class Fan {
+	private String make;
+	private double radius;
+	private String color;
+	private boolean isOn;
+	private byte speed;
+	
+	
+	public Fan(String make, double radius, String color) {
+		this.make = make;
+		this.radius = radius;
+		this.color = color;
+		
+	}
+	
+public String toString() {
+//		return make+" "+radius+" "+ color;
+	return String.format("make - %s, radius - %f, color - %s, isOn -%b, speed - %d",
+			make, radius, color, isOn, speed);
+	}
+}
