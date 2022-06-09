5acd57b622b8bcc20ad4463c5779cf8a1ebe1dea
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-09 15:28:37 +0530

    add list

--- ArrayListConcept/src/com/masai/vectorlist/ListConcept.java
+++ ArrayListConcept/src/com/masai/vectorlist/ListConcept.java
@@ -1,9 +1,23 @@
 package com.masai.vectorlist;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 public class ListConcept {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		List<String> words = List.of("Apple", "Cat", "Bat");
+		
+		List<String> vector = new Vector<String>(words);
+		List<String> arrList = new ArrayList<String>(words);
+		
+		
+		arrList.add("Elephant");
+		System.out.println("Vector List-> "+ vector);
+		System.out.println(arrList);
 
 	}
 
