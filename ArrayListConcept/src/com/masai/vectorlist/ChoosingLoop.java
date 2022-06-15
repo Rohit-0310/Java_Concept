fda84e7845807e2a04acd6cf26bb9421bb48d82c
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-15 11:39:52 +0530

    ArrayList

--- ArrayListConcept/src/com/masai/vectorlist/ChoosingLoop.java
+++ ArrayListConcept/src/com/masai/vectorlist/ChoosingLoop.java
@@ -1,6 +1,7 @@
 package com.masai.vectorlist;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ChoosingLoop {
@@ -12,17 +13,36 @@ public class ChoosingLoop {
 		List<String> arrList = new ArrayList<String>(words);
 		System.out.println(arrList);
 
-		//Print thosewords ending with at
+		//Print those words ending with at
 		
 		for(String word : words) 
 		{
 			if(word.endsWith("at")) 
 			{
-				System.out.println(word);
+				System.out.println("ends With at-> " + word);
 			}
 		}
 		
+//		//Delete all the values which ends with at
+//		//For Removing we cannot use this loop
+//		for(String word : arrList) 
+//		{
+//			if(word.endsWith("at")) 
+//			{
+//				arrList.remove(word);
+//			}
+//		}
+//		System.out.println(arrList.toString());
+		
 
+		Iterator<String> iterator = arrList.iterator();
+		while(iterator.hasNext())
+		{
+			if(iterator.next().endsWith("at")) {
+				iterator.remove();
+			}
+		}
+		System.out.println("Iterator-> " + iterator);
 	}
 
 }
